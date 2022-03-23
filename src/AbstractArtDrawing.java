4be1176ed13190ffd886d5b7e7130bf910ab8b32
nir koren <nirkoren1@gmail.com>
2022-03-23 16:21:04 +0200

    .

--- src/AbstractArtDrawing.java
+++ src/AbstractArtDrawing.java
@@ -7,5 +7,24 @@ import java.util.Random;
 import java.awt.Color;
 
 public class AbstractArtDrawing {
+    public static final int WIDTH = 400;
+    public static final int HEIGHT  = 300;
 
+    public void createRandomLine() {
+        Random rand = new Random();
+
+    }
+
+    public void drawLineToSurface(DrawSurface sur) {
+
+    }
+
+    public void drawRandomLines() {
+
+    }
+
+    public static void main(String[] args) {
+        AbstractArtDrawing gui = new AbstractArtDrawing();
+        gui.drawRandomLines();
+    }
 }
