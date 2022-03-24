0cb64bbd12b25a480fa17e80b36d39ed1ffd61ca
nir koren <nirkoren1@gmail.com>
2022-03-24 19:19:00 +0200

    .

--- src/AbstractArtDrawing.java
+++ src/AbstractArtDrawing.java
@@ -10,6 +10,7 @@ public class AbstractArtDrawing {
     public static final int WIDTH = 400;
     public static final int HEIGHT  = 300;
     public static final int NUMOFLINES  = 20;
+    public static final int RADIUS  = 3;
 
     public Line createRandomLine() {
         Random rand = new Random();
@@ -19,12 +20,19 @@ public class AbstractArtDrawing {
     public void drawLineToSurface(DrawSurface sur, Line line) {
         sur.drawLine((int) line.start().getX(), (int) line.start().getY(), (int) line.end().getX(),
                 (int) line.end().getY());
-        sur.drawCircle();
-        sur.drawCircle();
-        sur.drawCircle();
     }
 
-    public void drawIntersections ()
+    public void drawCircles(DrawSurface sur, Line[] lines) {
+        for (int i = 0; i < NUMOFLINES; i++) {
+            Point mid = lines[i].middle();
+            sur.drawCircle((int) mid.getX(), (int) mid.getY(), RADIUS);
+            for (int j = 0; j < NUMOFLINES; j++) {
+                if (j == i) {
+                    continue;
+                }
+            }
+        }
+    };
 
     public void drawRandomLines() {
         GUI gui = new GUI("Random Circles Example", WIDTH, HEIGHT);
