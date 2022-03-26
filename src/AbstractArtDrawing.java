c2c767e8caaa527f5898adf4dd756f0d93d782e3
Nir <nirkoren1@gmail.com>
2022-03-26 16:54:05 +0300

    .

--- src/AbstractArtDrawing.java
+++ src/AbstractArtDrawing.java
@@ -2,26 +2,39 @@
 
 import biuoop.GUI;
 import biuoop.DrawSurface;
-
 import java.util.Random;
 import java.awt.Color;
 
+/**
+ * This class draws a modern art using lines and circles.
+ */
 public class AbstractArtDrawing {
     public static final int WIDTH = 400;
     public static final int HEIGHT  = 300;
     public static final int NUMOFLINES  = 10;
     public static final int RADIUS  = 3;
-
+    /**
+     * create a random line.
+     * @return the random line
+     */
     public Line createRandomLine() {
         Random rand = new Random();
         return new Line(rand.nextInt(WIDTH), rand.nextInt(HEIGHT), rand.nextInt(WIDTH), rand.nextInt(HEIGHT));
     }
-
+    /**
+     * draw a line to a surface.
+     * @param sur the surface which the line will be drawn on to
+     * @param line a Line object
+     */
     public void drawLineToSurface(DrawSurface sur, Line line) {
         sur.drawLine((int) line.start().getX(), (int) line.start().getY(), (int) line.end().getX(),
                 (int) line.end().getY());
     }
-
+    /**
+     * draw the locations of the intersections and the middle points of the lines.
+     * @param sur the surface which the line will be drawn on to
+     * @param lines Line objects array
+     */
     public void drawCircles(DrawSurface sur, Line[] lines) {
         for (int i = 0; i < NUMOFLINES - 1; i++) {
             Point mid = lines[i].middle();
@@ -36,8 +49,10 @@ public class AbstractArtDrawing {
                 sur.fillCircle((int) intersection.getX(), (int) intersection.getY(), RADIUS);
             }
         }
-    };
-
+    }
+    /**
+     * create random lines than draw them to a surface and draw the intersection points and middle points of the lines.
+     */
     public void drawRandomLines() {
         GUI gui = new GUI("Abstract Art Drawing", WIDTH, HEIGHT);
         DrawSurface sur = gui.getDrawSurface();
@@ -49,7 +64,10 @@ public class AbstractArtDrawing {
         drawCircles(sur, lines);
         gui.show(sur);
     }
-
+    /**
+     * the main method create a new AbstractArtDrawing object and calling the drawRandomLines method.
+     * @param args String array provided by the cmd
+     */
     public static void main(String[] args) {
         AbstractArtDrawing gui = new AbstractArtDrawing();
         gui.drawRandomLines();
--- src/Ball.java
+++ src/Ball.java
@@ -3,11 +3,28 @@
 import biuoop.DrawSurface;
 import java.awt.Color;
 
+/**
+ * this class represents a ball.
+ * a ball have Velocity, a center point, size (radius) and color.
+ */
 public class Ball {
     private Integer radius = null;
     private Color color = null;
     private Point center = new Point(0, 0);
     private Velocity velocity = new Velocity(0, 0);
+
+    /**
+     * the constructor.
+     * the constructor assign all the members of the Ball.
+     * it prevent the ball from starting outside the frame boundaries.
+     * @param center Point the center point of the ball to start
+     * @param r radius
+     * @param color java.awt.Color color
+     * @param xStart int the x start of the frame
+     * @param yStart int the y start of the frame
+     * @param xEnd int the x end of the frame
+     * @param yEnd int the y end of the frame
+     */
     public Ball(Point center, int r, java.awt.Color color, int xStart, int yStart, int xEnd, int yEnd) {
         int x = Math.min(Math.max((int) center.getX(), r + xStart), xEnd - r);
         int y = Math.min(Math.max((int) center.getY(), r + yStart), yEnd - r);
@@ -17,27 +34,63 @@ public class Ball {
         this.color = color;
     }
 
+    /**
+     * @return center x value
+     */
     public int getX() {
         return (int) this.center.getX();
-    };
+    }
+    /**
+     * @return center y value
+     */
     public int getY() {
         return (int) this.center.getY();
     }
+    /**
+     * @return size value
+     */
     public int getSize() {
         return this.radius;
     }
+    /**
+     * @return java.awt.Color color
+     */
     public java.awt.Color getColor() {
         return this.color;
     }
+
+    /**
+     * set velocity from Velocity object.
+     * using the setVelocity method
+     * @param v Velocity object
+     */
     public void setVelocity(Velocity v) {
         this.velocity.setValues(v.getDx(), v.getDy());
     }
+
+    /**
+     * set velocity from dx and dy values.
+     * @param dx x-axis speed
+     * @param dy y-axis speed
+     */
     public void setVelocity(double dx, double dy) {
         this.velocity.setValues(dx, dy);
     }
+
+    /**
+     * @return Velocity object of the ball
+     */
     public Velocity getVelocity() {
         return this.velocity;
     }
+
+    /**
+     * change the direction of the speed if the ball hits the frame boundaries.
+     * @param xStart x start of the frame
+     * @param yStart y start of the frame
+     * @param xEnd x end of the frame
+     * @param yEnd y end of the frame
+     */
     public void moveOneStep(int xStart, int yStart, int xEnd, int yEnd) {
         this.center = this.getVelocity().applyToPoint(this.center);
         if (this.getX() + this.velocity.getDx() + this.radius >= xEnd
@@ -49,7 +102,11 @@ public class Ball {
             this.setVelocity(this.velocity.getDx(), -this.velocity.getDy());
         }
     }
-    // draw the ball on the given DrawSurface
+
+    /**
+     * draw ball of a surface.
+     * @param surface a surface to draw the ball on
+     */
     public void drawOn(DrawSurface surface) {
         surface.setColor(this.color);
         surface.fillCircle((int) center.getX(), (int) center.getY(), this.radius);
