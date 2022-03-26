aeed46e47e1ef867a80b25dd1b5eb7b06c80043c
Nir <nirkoren1@gmail.com>
2022-03-26 18:01:55 +0300

    .

--- src/MultipleFramesBouncingBallsAnimation.java
+++ src/MultipleFramesBouncingBallsAnimation.java
@@ -8,6 +8,10 @@ import java.awt.Color;
 import java.util.Arrays;
 import java.util.Random;
 
+/**
+ * this class create two frames and multiple Balls, then bounce them in the two frames.
+ * this class contains the frames boundaries and the width and height of the window.
+ */
 public class MultipleFramesBouncingBallsAnimation {
     public static final int FRAME1XSTART = 50;
     public static final int FRAME1YSTART = 50;
@@ -19,6 +23,17 @@ public class MultipleFramesBouncingBallsAnimation {
     public static final int FRAME2YEND = 600;
     public static final int WIDTH = 600;
     public static final int HEIGHT = 600;
+
+    /**
+     * create a new Ball.
+     * calculating the speed of the ball according to his size (bigger are slower, slowest are 50+)
+     * @param size the size of the ball (radius)
+     * @param xStart start x of the frame
+     * @param yStart start y of the frame
+     * @param xEnd end x of the frame
+     * @param yEnd end y of the frame
+     * @return Ball object
+     */
     static Ball addBall(int size, int xStart, int yStart, int xEnd, int yEnd) {
         Random random = new Random();
         Point start = new Point(random.nextInt(xStart, xEnd), random.nextInt(yStart, yEnd));
@@ -28,24 +43,37 @@ public class MultipleFramesBouncingBallsAnimation {
         ball.setVelocity(vel.getDx(), vel.getDy());
         return ball;
     }
+
+    /**
+     * crete a random color.
+     * @return java.awt.Color obj
+     */
     static Color getRandomColor() {
         Random random = new Random();
         Color[] colors = {Color.RED, Color.BLACK, Color.orange, Color.GREEN, Color.BLUE, Color.MAGENTA};
         return colors[random.nextInt(colors.length)];
     }
-    static private void drawAnimation(int[] ballsSizes) {
-        Arrays.sort(ballsSizes);
-        System.out.println(ballsSizes[0]);
-        GUI gui = new GUI("Bouncing Balls!", WIDTH, HEIGHT);
+
+    /**
+     * create an array of balls, then move all of them in a loop.
+     * drawing two frames, the first half of the balls bounce in the first frame and the second half in the second
+     * frame.
+     * @param ballsSizes int array, the sizes of the balls
+     */
+    private static void drawAnimation(int[] ballsSizes) {
+        Arrays.sort(ballsSizes, 0, ballsSizes.length / 2);   // sorting so that smaller balls are on front
+        Arrays.sort(ballsSizes, ballsSizes.length / 2, ballsSizes.length); // sorting so that smaller balls are on front
+        GUI gui = new GUI("Bouncing Balls With Frames!", WIDTH, HEIGHT);
         Sleeper sleeper = new Sleeper();
         Ball[] balls = new Ball[ballsSizes.length];
         for (int i = 0; i < ballsSizes.length; i++) {
-            if (i <= balls.length / 2) {
-                balls[i] = addBall(ballsSizes[balls.length - i - 1], FRAME1XSTART, FRAME1YSTART, FRAME1XEND,
+            if (i < balls.length / 2) {
+                balls[i] = addBall(ballsSizes[balls.length / 2 - i - 1], FRAME1XSTART, FRAME1YSTART, FRAME1XEND,
                         FRAME1YEND);
                 continue;
             }
-            balls[i] = addBall(ballsSizes[balls.length - i - 1], FRAME2XSTART, FRAME2YSTART, FRAME2XEND, FRAME2YEND);
+            balls[i] = addBall(ballsSizes[balls.length / 2 - i - 1 + balls.length], FRAME2XSTART, FRAME2YSTART,
+                    FRAME2XEND, FRAME2YEND);
         }
         while (true) {
             DrawSurface d = gui.getDrawSurface();
@@ -54,10 +82,10 @@ public class MultipleFramesBouncingBallsAnimation {
             d.setColor(Color.YELLOW);
             d.fillRectangle(FRAME2XSTART, FRAME2YSTART, FRAME2XEND - FRAME2XSTART, FRAME2YEND - FRAME2YSTART);
             for (int i = 0; i < ballsSizes.length; i++) {
-                if (i <= balls.length / 2) {
-                    balls[i].moveOneStep(FRAME1XSTART, FRAME1YSTART, FRAME1XEND, FRAME1YEND);
+                if (i < balls.length / 2) {
+                    balls[i].moveOneStep(FRAME1XSTART, FRAME1YSTART, FRAME1XEND, FRAME1YEND);  //first half
                 } else {
-                    balls[i].moveOneStep(FRAME2XSTART, FRAME2YSTART, FRAME2XEND, FRAME2YEND);
+                    balls[i].moveOneStep(FRAME2XSTART, FRAME2YSTART, FRAME2XEND, FRAME2YEND);  //second half
                 }
                 balls[i].drawOn(d);
             }
@@ -66,6 +94,10 @@ public class MultipleFramesBouncingBallsAnimation {
         }
     }
 
+    /**
+     * this main function takes balls sizes from the cmd and calls the drawAnimation method.
+     * @param args String array - the sizes of the balls
+     */
     public static void main(String[] args) {
         int[] sizes = new int[args.length];
         for (int i = 0; i < args.length; i++) {
